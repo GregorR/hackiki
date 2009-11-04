43262b75117dbf525f0f1ceaa0614e80f28dd945
Gregor Richards <Richards@codu.org>
2009-11-04 02:25:35 +0000

    HACKIKI_DIR no longer is meaningful.

--- lib/config.php
+++ lib/config.php
@@ -1,7 +1,7 @@
 <?PHP
 $hackiki_path = "/var/lib/hackiki";
 $wiki_base = "/wiki";
-$env_ok = array("PATH", "HACKIKI_BASE", "HACKIKI_DIR");
+$env_ok = array("PATH", "HACKIKI_BASE");
 
 $hackiki_fs_path = "$hackiki_path/fs";
 ?>
