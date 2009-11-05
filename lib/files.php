84df66f1fc7821552dd3110f0818fc81bba53f38
Gregor Richards <Richards@codu.org>
2009-11-05 17:00:53 -0500

    Added support for uploaded files.

--- lib/files.php
+++ lib/files.php
@@ -21,48 +21,14 @@
  * THE SOFTWARE.
  */
 
-require_once("lib/env.php");
-require_once("lib/files.php");
-
-function polanice($fsdir, $cmd) {
-    global $hackiki_path;
-
-    $POLA_OPTS = "";
-    if (file_exists("/lib64")) {
-        $POLA_OPTS="$POLA_OPTS -f=/lib64";
+function handleFiles($tmpdir) {
+    $FILES="";
+    foreach ($_FILES as $file) {
+        $nm = basename($file["tmp_name"]);
+        rename($file["tmp_name"], $tmpdir . "/" . $nm);
+        $FILES .= " $nm";
+        putenv("FILE_$nm=" . $file["name"]);
     }
-    if (file_exists("/etc/alternatives")) {
-        $POLA_OPTS="$POLA_OPTS -f=/etc/alternatives";
-    }
-
-    $tmpf = tempnam("/tmp", "tmpdir.");
-    $tmpd = $tmpf . ".d";
-    mkdir($tmpd);
-    unlink($tmpf);
-
-    $oldpath = $_ENV["PATH"];
-    putenv("PATH=/hackiki/bin:/bin:/usr/bin");
-    cleanenv();
-    handleFiles();
-
-    // read the output
-    $cmdh = popen(
-        "/usr/bin/pola-run -B $POLA_OPTS -tw /tmp $tmpd -tw /hackiki $fsdir " .
-        "--cwd / " .
-        "-f=$hackiki_path/limits --prog=$hackiki_path/limits " .
-        "-fa=/usr/bin/nice -a=-n10 " .
-        "-e $cmd 2>&1", "r");
-    $outp = "";
-    if ($cmdh !== false) {
-        while (!feof($cmdh)) {
-            $outp .= fread($cmdh, 8192);
-        }
-    }
-
-    putenv("PATH=$oldpath");
-
-    exec("rm -rf $tmpd");
-
-    return $outp;
+    putenv("FILES=$FILES");
 }
 ?>
