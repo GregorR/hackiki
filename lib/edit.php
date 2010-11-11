9be75dfd29cc87eb4c14e2009c5f1ead8212410d
Gregor Richards <Richards@codu.org>
2010-11-11 22:54:18 +0000

    No, it should be "No"

--- lib/edit.php
+++ lib/edit.php
@@ -29,7 +29,7 @@ function performEdit($fsdir, $args) {
     $fnam = $fsdir . "/" . $fshr;
 
     // prepare our output
-    $html = "headers\nX-Hackiki-Cached: no\n\n" .
+    $html = "headers\nX-Hackiki-Cached: No\n\n" .
             "<html><head><title>Edit: $fshr</title>" .
             "<meta http-equiv=\"Content-type\" value=\"text/html; charset=utf-8\" /></head><body>";
 
